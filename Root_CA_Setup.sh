90fbd84f80420b44a4bd7cfd45517ddbaf445ec8
Haydn <hje619@gmail.com>
2020-12-21 18:02:13 +0100

    added no prompt

--- Root_CA_Setup.sh
+++ Root_CA_Setup.sh
@@ -3,8 +3,8 @@
 if [[ -n "$(find /tmp/cacerts/ -iname *.crt)" ]]; then
 	for f in /tmp/cacerts/*; do
 		case "$f" in
-				*.crt)     $JAVA_HOME/bin/keytool -import -alias $f -keystore $JAVA_HOME/lib/security/cacerts -file "/tmp/cacerts/$f.crt" ;;
-				*.cer)     $JAVA_HOME/bin/keytool -import -alias $f -keystore $JAVA_HOME/lib/security/cacerts -file "/tmp/cacerts/$f.cer" ;;
+				*.crt)     $JAVA_HOME/bin/keytool -importcert -noprompt -alias $f -keystore $JAVA_HOME/lib/security/cacerts -file "/tmp/cacerts/$f.crt" ;;
+				*.cer)     $JAVA_HOME/bin/keytool -importcert -noprompt -alias $f -keystore $JAVA_HOME/lib/security/cacerts -file "/tmp/cacerts/$f.cer" ;;
 				*)         echo "$0: ignoring $f" ;;
 
 		esac
